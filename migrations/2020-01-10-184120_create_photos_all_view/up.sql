f3da6c58321cdd92b52e853454bda5d797d59053
Nathaniel Ledford <nate@nateledford.com>
2020-01-16 07:51:56 -0500

    Fix issues in photos_all view

--- migrations/2020-01-10-184120_create_photos_all_view/up.sql
+++ migrations/2020-01-10-184120_create_photos_all_view/up.sql
@@ -2,7 +2,7 @@
 -- This view will aggregate data from junction tables as text arrays using left joins
 -- The process for constucting the query can be found from this StackOverflow answer: https://dba.stackexchange.com/a/173879
 
-create view photos_all as
+create or replace view photos_all as
 select id,
        file_path,
        replace(file_path, file_name, '') folder,
@@ -24,16 +24,16 @@ from photos p
     select pe.photo_id as id, array_agg(e.entity_name) as entities
     from photo_entity pe
              JOIN entity e on pe.entity_id = e.id
-    group by pe.id) e using (id)
+    group by pe.photo_id) e using (id)
          LEFT JOIN (
     SELECT pt.photo_id as id, array_agg(t.tag_name) as tags
     FROM photo_tag pt
              JOIN tags t on pt.tag_id = t.id
-    GROUP BY pt.id
+    GROUP BY pt.photo_id
 ) t using (id)
          LEFT JOIN (
     SELECT pw.photo_id as id, array_agg(ws.name) as wallpapers
     FROM photo_wallpaper pw
              JOIN wallpaper_sizes ws on pw.wallpaper_size_id = ws.id
-    GROUP BY pw.id
+    GROUP BY pw.photo_id
 ) w using (id);
