50f7a2cd3f4ec38b0d0a8284f5cba57c5894eb30
Nathaniel Ledford <nate@nateledford.com>
2020-01-17 06:33:47 -0500

    Cleanup code

--- src/handlers/tags.rs
+++ src/handlers/tags.rs
@@ -1,16 +1,16 @@
-use actix_web::{get, HttpResponse, web};
+use actix_web::{get, web, HttpResponse};
 use deadpool_postgres::Pool;
 
 use crate::models::errors;
-use crate::schemas::DbTable;
-use crate::schemas::tags::Tag;
 use crate::models::responses::ApiResponse;
+use crate::schemas::tags::Tag;
+use crate::schemas::DbTable;
 
 // ALL TAGS ****************************************************************************************
 
 #[get("/tags")]
 pub async fn get_tags(pool: web::Data<Pool>) -> Result<HttpResponse, errors::Error> {
-    let tags = Tag::get_all(&pool).await;
+    let res = Tag::get_all(&pool).await;
 
     match res {
         Ok(tags) => Ok(HttpResponse::Ok().json(ApiResponse::new("success", 200, "Ok", tags))),
