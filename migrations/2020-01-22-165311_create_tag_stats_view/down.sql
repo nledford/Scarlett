b6b2c3bff3c64bf11c807cedc0d325a911c544a7
Nathaniel Ledford <nate@nateledford.com>
2020-01-22 11:55:07 -0500

    Add tag stats view

---
+++ migrations/2020-01-22-165311_create_tag_stats_view/down.sql
@@ -0,0 +1,1 @@
+drop view if exists tag_stats;
---
+++ migrations/2020-01-22-165311_create_tag_stats_view/up.sql
@@ -0,0 +1,16 @@
+-- displays metadata about tags
+create or replace view tag_stats as
+select tag_name,
+       photos_with_tag,
+       (photos_with_tag::numeric / photos_with_tags::numeric) * 100                  percentage_with_tag,
+       (photos_with_tag::numeric / (select count(*)::numeric from photos_all)) * 100 percentage_total
+from (select t.tag_name,
+             (select count(pt.photo_id)
+              from tags t2
+                       left join photo_tag pt on t2.id = pt.tag_id
+              where t2.id = t.id)                                      photos_with_tag,
+             (select count(distinct photo_id)
+              from tags t3
+                       inner join photo_tag pt2 on t3.id = pt2.tag_id) photos_with_tags
+      from tags t) s
+order by photos_with_tag desc, tag_name;
