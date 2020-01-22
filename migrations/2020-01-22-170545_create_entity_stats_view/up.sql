465960fbf4f8a6e606d88cfc0ad5b5cb6e234ab0
Nathaniel Ledford <nate@nateledford.com>
2020-01-22 13:33:31 -0500

    Only calculate entity stats with non-anonymous photos

--- migrations/2020-01-22-170545_create_entity_stats_view/up.sql
+++ migrations/2020-01-22-170545_create_entity_stats_view/up.sql
@@ -8,9 +8,11 @@ from (select se.entity_name,
              (select count(pe.photo_id)
               from sorted_entity se2
                        left join photo_entity pe on se2.id = pe.entity_id
-              where se2.id = se.id)                                       photos_with_entity,
+              where se2.id = se.id)              photos_with_entity,
              (select count(distinct photo_id)
               from sorted_entity se3
-                       inner join photo_entity p on se3.id = p.entity_id) photos_with_entities
+                       inner join photo_entity pe on se3.id = pe.entity_id
+                       inner join photos_all pa on pa.id = pe.photo_id
+              where anonymous_entities is false) photos_with_entities
       from sorted_entity se) s
-order by photos_with_entity desc, sort_name;
+
