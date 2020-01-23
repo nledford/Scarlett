e0e4820ee462247268da5ecfa87347a30d9f688c
Nathaniel Ledford <nate@nateledford.com>
2020-01-23 14:11:50 -0500

    Clean up code

--- src/handlers/collections.rs
+++ src/handlers/collections.rs
@@ -1,9 +1,9 @@
-use actix_web::{HttpResponse, web, get, post, patch, delete};
 use crate::errors::errors;
-use deadpool_postgres::Pool;
+use crate::responses::api_response::ApiResponse;
 use crate::schemas::collections::Collection;
 use crate::schemas::DbTable;
-use crate::responses::api_response::ApiResponse;
+use actix_web::{delete, get, patch, post, web, HttpResponse};
+use deadpool_postgres::Pool;
 
 // ALL COLLECTIONS *********************************************************************************
 
@@ -13,19 +13,22 @@ pub async fn get_collections(pool: web::Data<Pool>) -> Result<HttpResponse, erro
 
     match res {
         Ok(collections) => Ok(ApiResponse::success(collections)),
-        Err(err) => Ok(ApiResponse::error(err.to_string()))
+        Err(err) => Ok(ApiResponse::error(err.to_string())),
     }
 }
 
 // SINGLE COLLECTION *******************************************************************************
 
 #[get("/collections/{id}")]
-pub async fn get_collection(info: web::Path<i32>, pool: web::Data<Pool>) -> Result<HttpResponse, errors::Error> {
+pub async fn get_collection(
+    info: web::Path<i32>,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, errors::Error> {
     let res = Collection::get_by_id(info.into_inner(), &pool).await;
 
     match res {
         Ok(collection) => Ok(ApiResponse::success(collection)),
-        Err(err) => Ok(ApiResponse::error(err.to_string()))
+        Err(err) => Ok(ApiResponse::error(err.to_string())),
     }
 }
 
@@ -38,37 +41,47 @@ pub struct NewCollection {
 }
 
 #[post("/collections")]
-pub async fn create_collection(params: web::Json<NewCollection>, pool: web::Data<Pool>) -> Result<HttpResponse, errors::Error> {
+pub async fn create_collection(
+    params: web::Json<NewCollection>,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, errors::Error> {
     let collection = params.into_inner();
 
     let res = Collection::create(&collection.name, &collection.query, &pool).await;
 
     match res {
         Ok(new_collection) => Ok(ApiResponse::success(new_collection)),
-        Err(err) => Ok(ApiResponse::error(err.to_string()))
+        Err(err) => Ok(ApiResponse::error(err.to_string())),
     }
 }
 
 // UPDATE COLLECTION *******************************************************************************
 
 #[patch("/collections/{id}")]
-pub async fn update_collection(_: web::Path<i32>, params: web::Json<Collection>, pool: web::Data<Pool>) -> Result<HttpResponse, errors::Error> {
+pub async fn update_collection(
+    _: web::Path<i32>,
+    params: web::Json<Collection>,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, errors::Error> {
     let res = Collection::update(params.into_inner(), &pool).await;
 
     match res {
         Ok(updated_collection) => Ok(ApiResponse::success(updated_collection)),
-        Err(err) => Ok(ApiResponse::error(err.to_string()))
+        Err(err) => Ok(ApiResponse::error(err.to_string())),
     }
 }
 
 // DELETE COLLECTION *******************************************************************************
 
 #[delete("/collections/{id}")]
-pub async fn delete_collection(info: web::Path<i32>, pool: web::Data<Pool>) -> Result<HttpResponse, errors::Error> {
+pub async fn delete_collection(
+    info: web::Path<i32>,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse, errors::Error> {
     let res = Collection::delete(info.into_inner(), &pool).await;
 
     match res {
         Ok(message) => Ok(ApiResponse::success(message)),
-        Err(err) => Ok(ApiResponse::error(err.to_string()))
+        Err(err) => Ok(ApiResponse::error(err.to_string())),
     }
 }
