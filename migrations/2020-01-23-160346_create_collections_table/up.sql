7db857e9f8ec94e9f29d7b840e89c25f0081bd4c
Nathaniel Ledford <nate@nateledford.com>
2020-01-23 11:16:19 -0500

    create collections table

---
+++ migrations/2020-01-23-160346_create_collections_table/up.sql
@@ -0,0 +1,15 @@
+-- create the collections table
+-- this will act as a "smart collections" table. Rather than manually specifying which pictures belong to a collection,
+-- we'll store a partial WHERE clause that describes what images we want to retrieve for a specific collection
+
+-- ideally I'd use a query builder on the client application and store a json/xml with the where clause but this will
+-- at least get me started in building smart collections
+
+create table collections
+(
+    id    bigserial     not null
+        constraint collections_pk
+            primary key,
+    name  varchar(50)   not null,
+    query varchar(1000) not null
+);
