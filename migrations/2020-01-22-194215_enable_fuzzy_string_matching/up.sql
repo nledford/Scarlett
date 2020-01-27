3af182a1d58a16b9e57e813dc83fe2915dc7f381
Nathaniel Ledford <nate@nateledford.com>
2020-01-27 10:55:01 -0500

    Manually reorder migrations

--- migrations/2020-01-22-194215_enable_fuzzy_string_matching/up.sql
+++
@@ -1,8 +0,0 @@
--- Enables trigrams in Postgresql
--- allows for use of fuzzy string matching
--- SEE: https://www.freecodecamp.org/news/fuzzy-string-matching-with-postgresql/
-
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-
--- example for fuzzy string matching entities:
--- `select * from sorted_entity order by similarity(entity_name, 'M Ash') desc;`
