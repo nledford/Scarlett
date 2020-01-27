964438ebbdfb9c5a46e41fbd1912c5c86c017dc1
Nathaniel Ledford <nate@nateledford.com>
2020-01-27 09:46:31 -0500

    Add constraints and indexes to tables

--- migrations/2020-01-10-180026_tags_tables/up.sql
+++ migrations/2020-01-10-180026_tags_tables/up.sql
@@ -1,11 +1,17 @@
 -- Add tags table
--- Each row simply holds a single tag
+-- Each row simply holds a single tag which must be lowercase
 create table tags
 (
     id       serial       not null
         constraint tags_pk primary key,
     tag_name varchar(100) not null
+        constraint lowercase_tag_name
+            check ( tag_name = lower(tag_name) )
 );
+
+-- ensure there cannot be duplicate tag names
+create unique index idx_lower_tag_name_unique
+    on tags (lower(tag_name));
 
 -- Add `photo_tag` junction table
 create table photo_tag
@@ -17,3 +23,7 @@ create table photo_tag
     constraint photo_tag_photos_fk foreign key (photo_id) references photos (id),
     constraint photo_tag_tags_fk foreign key (tag_id) references tags (id)
 );
+
+-- ensure there cannot be duplicate photo/tag combinations
+create unique index idx_unique_photo_tag_combo
+    on photo_tag (photo_id, tag_id);
