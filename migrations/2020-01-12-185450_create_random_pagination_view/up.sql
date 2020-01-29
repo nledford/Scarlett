de89594eac9127cde449077eadd99a5e1637049f
Nathaniel Ledford <nate@nateledford.com>
2020-01-29 13:11:22 -0500

    Overkill random ordering with uuids

--- migrations/2020-01-12-185450_create_random_pagination_view/up.sql
+++ migrations/2020-01-12-185450_create_random_pagination_view/up.sql
@@ -2,13 +2,12 @@
 -- SOURCE: https://schinckel.net/2019/11/08/random%2C-fixed-ordering-and-pagination/
 
 CREATE MATERIALIZED VIEW photo_ordering AS
-
 SELECT photo.id             AS photo_id,
        row_number() over () AS position
 FROM (
          SELECT id
          FROM photos
-         ORDER BY random()
+         ORDER BY gen_random_uuid()
      ) photo;
 
 CREATE INDEX photo_ordering_id ON photo_ordering (photo_id);
