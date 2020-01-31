83144337d3305f56670bd165110635ca9382a0de
Nathaniel Ledford <nate@nateledford.com>
2020-01-31 13:11:05 -0500

    Fix issues with sorted entity view

--- migrations/2020-01-15-182550_create_sorted_entities_view/up.sql
+++ migrations/2020-01-15-182550_create_sorted_entities_view/up.sql
@@ -1,16 +1,25 @@
 -- View adds a column that reconstructs the `entity_name` column into last name, first name
 
-create view sorted_entity as
+create or replace view sorted_entity as
 select id,
        entity_name,
-       name_arr[array_upper(name_arr, 1)]
-           || ', '
-           || replace(entity_name, name_arr[array_upper(name_arr, 1)], '') sort_name,
+       case
+           when cardinality(name_arr) = 1 then name_arr[1]
+           else
+                       name_arr[array_upper(name_arr, 1)]
+                       || ', '
+                   || replace(entity_name, name_arr[array_upper(name_arr, 1)], '') end sort_name,
        alternate_names,
        instagram_username,
        twitter_username,
        favorite,
        profile_photo_id
-from (select e.*, string_to_array(e.entity_name, ' ') name_arr
+from (select e.*,
+             case
+                 when entity_name like '%[%]%' then string_to_array(trim(split_part(entity_name, '[', 1)), ' ')
+                 when entity_name like '%(%)%' then string_to_array(trim(split_part(entity_name, '(', 1)), ' ')
+                 when entity_name ~ '[A-Za-z]+[ ][A-Za-z]+[ ]\d'
+                     then string_to_array(trim(regexp_replace(entity_name, '[ ]\d+', '')), ' ')
+                 else string_to_array(trim(entity_name), ' ') end name_arr
       from entity e) v
 order by sort_name;
