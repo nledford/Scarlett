91d3e150fb0ddc6337a2a537d18dc595ede77b0a
Nathaniel Ledford <nate@nateledford.com>
2020-02-05 18:35:02 -0500

    Add pagination types

--- src/types.rs
+++ src/types.rs
@@ -2,7 +2,11 @@ use actix_web::{Error, HttpResponse};
 
 use crate::pagination::page::Page;
 use crate::responses::api_response::ApiResponse;
+use crate::schemas::entity::Entity;
 use crate::schemas::photo_full::PhotoFull;
+use crate::schemas::tags::Tag;
+use crate::stats::entities::EntityStats;
+use crate::stats::tags::TagStats;
 
 // API RESPONSES ***********************************************************************************
 
@@ -11,6 +15,10 @@ pub type PaginatedPhotoResponse = ApiResponse<PaginatedPhotos>;
 // PAGINATION **************************************************************************************
 
 pub type PaginatedPhotos = Page<Vec<PhotoFull>>;
+pub type PaginatedEntities = Page<Vec<Entity>>;
+pub type PaginatedEntityStats = Page<Vec<EntityStats>>;
+pub type PaginatedTags = Page<Vec<Tag>>;
+pub type PaginatedTagStats = Page<Vec<TagStats>>;
 
 // RESULTS *****************************************************************************************
 
