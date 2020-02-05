461e5aad21b12efbe2477bb7dae10353b7d8cd6f
Nathaniel Ledford <nate@nateledford.com>
2020-02-05 13:32:00 -0500

    Fix divide by zero error

--- migrations/2020-01-22-165311_create_tag_stats_view/up.sql
+++ migrations/2020-01-22-165311_create_tag_stats_view/up.sql
@@ -2,10 +2,10 @@
 create or replace view tag_stats as
 select tag_name,
        photos_with_tag,
-       (photos_with_tag::decimal / greatest(photos_with_tags::decimal, 1)) * 100     percentage_with_tag,
+       (photos_with_tag::decimal / photos_with_tags::decimal) * 100                  percentage_with_tag,
        (photos_with_tag::decimal / (select count(*)::decimal from photos_all)) * 100 percentage_total
 from (select t.tag_name,
-             (select count(pt.photo_id)
+             (select nullif(count(pt.photo_id), 0)
               from tags t2
                        left join photo_tag pt on t2.id = pt.tag_id
               where t2.id = t.id)                                      photos_with_tag,
